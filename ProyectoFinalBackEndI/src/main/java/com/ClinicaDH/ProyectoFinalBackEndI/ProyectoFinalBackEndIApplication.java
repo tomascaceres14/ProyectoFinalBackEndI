b753cb30bcd3d41752118c457a29f29795176751
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-06-24 11:48:52 -0300

    primer commit

---
+++ ProyectoFinalBackEndI/src/main/java/com/ClinicaDH/ProyectoFinalBackEndI/ProyectoFinalBackEndIApplication.java
@@ -0,0 +1,13 @@
+package com.ClinicaDH.ProyectoFinalBackEndI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalBackEndIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalBackEndIApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinalBackEndI/src/test/java/com/ClinicaDH/ProyectoFinalBackEndI/ProyectoFinalBackEndIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClinicaDH.ProyectoFinalBackEndI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalBackEndIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
